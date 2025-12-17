af60599ab9a85203c46c4e02cf8e39bad5c3b289
FIDGGE <megamix0704@gmail.com>
2025-12-17 22:48:24 +0700

    Update laba.cpp

--- laba.cpp
+++ laba.cpp
@@ -1,52 +1,84 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 #include <vector>
 #include <map>
-
+#include <string>
 using namespace std;
 
 int main() {
   int year;
   
-  cout << " Enter Year ";
-  cin  >> year;
+  cout << "Enter Year: ";
+  cin >> year;
+  
+  cout << "\n\n";
   
   bool leapYear = false;
-    if (year % 4 == 0) {
-      if (year % 100 == 0) {
-        if (year % 400 == 0) {
-          leapYear = true;
-        }
-      } else {
-          leapYear = true;
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        leapYear = true;
       }
+    } else {
+      leapYear = true;
     }
+  }
+  
   string months[] = {
-  "January", "February", "March", "April",
-  "May", "June", "July", "August",
-  "September", "October", "November", "December"
-  };
-  string nameOfDay[] = {
-  "mon", "tue", "wed", "thu", "fri", "sat", "sun"
+    "january", "february", "march", "april",
+    "may", "june", "july", "august",
+    "september", "october", "november", "december"
   };
   
-  int days [] = {
-  31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  string nameOfDay[] = {
+    "mon", "tue", "wed", "thu", "fri", "sat", "sun"
+  };
+  
+  int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
   
   if (leapYear) {
     days[1] = 29;
   }
-  for (int month = 0; month < 12; ++month){
-    cout << "----------" << months[month] << "----------" << "\n";
-    cout << nameOfDay[0] << nameOfDay[1]  << nameOfDay[2];
-    for (int dayIndex = 1; dayIndex <= days[month]; ++dayIndex) {
-      cout << dayIndex << " ";
-      
-      if (dayIndex % 7 == 0) {
-        cout << "\n";
-      }
+  
+  int startDay[] = {0, 3, 4, 0, 2, 4, 0, 3, 6, 2, 4, 0};
+  
+  for (int monthIndex = 0; monthIndex < 12; ++monthIndex) {
+    cout << "|    | " << months[monthIndex] << "    |\n";
+    cout << "|----|----|----|----|----|----|----|\n";
+    
+    cout << "|";
+    for (int dayNameIndex = 0; dayNameIndex < 7; ++dayNameIndex) {
+      cout << nameOfDay[dayNameIndex] << " |";
     }
- 
-  cout << "\n\n";
+    cout << "\n";
+    cout << "|----|----|----|----|----|----|----|\n";
+    
+    int dayIndex = 1;
+    
+    for (int rowIndex = 0; rowIndex < 6; ++rowIndex) {
+      cout << "|";
+      
+      for (int columnIndex = 0; columnIndex < 7; ++columnIndex) {
+        if (rowIndex == 0 && columnIndex < startDay[monthIndex]) {
+          cout << "    |";
+        } else if (dayIndex <= days[monthIndex]) {
+          if (dayIndex < 10) {
+            cout << "  " << dayIndex << " |";
+          } else {
+            cout << " " << dayIndex << " |";
+          }
+          ++dayIndex;
+        } else {
+          cout << "    |";
+        }
+      }
+      
+      cout << "\n";
+      if (dayIndex > days[monthIndex]) break;
+    }
+    
+    cout << "\n\n";
   }
+  
+  return 0;
 }
